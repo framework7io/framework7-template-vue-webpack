2b8508c4f7820119e004aff78932cc3b346f03de
Henning Horn <hh@henninghorn.dk>
2017-01-10 16:54:08 +0100

    Fixed popup close link

--- src/app.vue
+++ src/app.vue
@@ -113,7 +113,7 @@
           <f7-page>
             <f7-navbar title="Popup">
               <f7-nav-right>
-                <f7-link close-popup>Close</f7-link>
+                <f7-link close-popup="#popup">Close</f7-link>
               </f7-nav-right>
             </f7-navbar>
             <f7-block>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, architecto. Cupiditate laudantium rem nesciunt numquam, ipsam. Voluptates omnis, a inventore atque ratione aliquam. Omnis iusto nemo quos ullam obcaecati, quod.</f7-block>
