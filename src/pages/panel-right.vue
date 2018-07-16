e91979420e1200f9fbbb8136da54430fa2722667
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-07-16 17:56:30 +0300

    Remove unneccesary props

--- src/pages/panel-right.vue
+++ src/pages/panel-right.vue
@@ -1,7 +1,7 @@
 <template>
   <f7-page>
-    <f7-navbar title="Right Panel" sliding></f7-navbar>
-    <f7-block>
+    <f7-navbar title="Right Panel"></f7-navbar>
+    <f7-block strong>
       <p>Right panel content goes here</p>
     </f7-block>
     <f7-block-title>Load page in panel</f7-block-title>
