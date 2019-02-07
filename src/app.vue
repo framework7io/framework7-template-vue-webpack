77b140f6ef04b15f1a1a2798e43b65676788d6f9
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-02-07 17:23:40 +0300

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -16,7 +16,7 @@
     </f7-panel>
 
     <!-- Main View -->
-    <f7-view id="main-view" url="/" main></f7-view>
+    <f7-view id="main-view" url="/" main class="safe-areas"></f7-view>
 
     <!-- Popup -->
     <f7-popup id="popup">
