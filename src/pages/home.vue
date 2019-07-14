d18e0c009d735c017d9eb6ce508948987339d228
Emanuele Coppola <EmanueleCoppola@users.noreply.github.com>
2019-07-14 16:36:01 +0200

    Changed icon-if-* with icon-*

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,11 +2,11 @@
   <f7-page>
     <f7-navbar>
       <f7-nav-left>
-        <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="left"></f7-link>
+        <f7-link icon-ios="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
       </f7-nav-left>
       <f7-nav-title>My App</f7-nav-title>
       <f7-nav-right>
-        <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="right"></f7-link>
+        <f7-link icon-ios="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
       </f7-nav-right>
     </f7-navbar>
     <f7-toolbar bottom>
